1b62f92c0aae6156b80c7101074f62747ddf404f
Roy Mor <roy.mor173@gmail.com>
2016-10-11 02:32:36 +0300

    feat: add / and /project routes and change the Vue instance mount element selector
    Add / route which renders the Main component with a child /project route which renders a dummy
    
    component for now.

--- src/app.js
+++ src/app.js
@@ -4,19 +4,30 @@ import './index.scss';
 import VueRouter from 'vue-router';
 Vue.use(VueRouter);
 
+import Main from './components/Main.vue';
+const Foo = {render: h => h('div', 'Hi')};
+
 const router = new VueRouter({
   mode: 'history',
   routes: [
     {
       path: '/',
-      components: {
-      }
+      name: 'main',
+      component: Main,
+      children: [
+        {
+          path: '/project',
+          name: 'project',
+          component: Foo
+        }
+      ],
+      redirect: '/project'
     }
   ]
 });
 
 export default new Vue({
-  el: '#root',
+  el: '#app',
   router,
   render: h => h('router-view')
 });
