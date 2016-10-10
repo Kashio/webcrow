fc2199d8d8a554a96678806cba98c2d90ff6ac1e
Roy Mor <roy.mor173@gmail.com>
2016-10-11 02:30:27 +0300

    feat(Main): add Main component
    Add Main component responsible for holding all the components that make the skeleton of the
    
    application

---
+++ src/components/Main.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="root">
+    <header-component></header-component>
+    <main>
+    </main>
+  </div>
+</template>
+
+<script>
+  import Header from './Header.vue';
+  export default {
+    name: 'Main',
+    components: {
+      'header-component': Header
+    }
+  };
+</script>
