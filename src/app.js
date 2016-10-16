f55b9f12aa9142e2c3f5d0a1fdbe2b42b576265f
Roy Mor <roy.mor173@gmail.com>
2016-10-16 20:53:49 +0300

    feat: make root vue to use the router file

--- src/app.js
+++ src/app.js
@@ -1,30 +1,8 @@
 import Vue from 'vue';
 
 import './index.scss';
-import VueRouter from 'vue-router';
-Vue.use(VueRouter);
 
-import Main from './components/Main.vue';
-const Foo = {render: h => h('div', 'Hi')};
-
-const router = new VueRouter({
-  mode: 'history',
-  routes: [
-    {
-      path: '/',
-      name: 'main',
-      component: Main,
-      children: [
-        {
-          path: '/project',
-          name: 'project',
-          component: Foo
-        }
-      ],
-      redirect: '/project'
-    }
-  ]
-});
+import { router } from './router/router';
 
 export default new Vue({
   el: '#app',
