64a33b01fc6a8946f7bf064af94f31b6fc96ffc1
Roy Mor <roy.mor173@gmail.com>
2016-10-16 20:56:57 +0300

    feat(Header): add Header component
    Header component responsible for displaying brand name and version

---
+++ src/components/Layout/Header/Header.vue
@@ -0,0 +1,56 @@
+<template>
+  <header v-once class="header">
+    <router-link to="/" class="brand">
+      <span class="brand-name">{{ name }}</span>
+      <span class="brand-version">{{ version }}</span>
+    </router-link>
+  </header>
+</template>
+
+<script>
+  import { name, version } from '../../../services/brand';
+  export default {
+    name: 'Header',
+    data() {
+      return {
+        name,
+        version
+      };
+    }
+  };
+</script>
+
+<style lang="sass" rel="stylesheet/scss" scoped>
+  $font-stack: 'Open Sans', sans-serif;
+  $primary-color: #2d5079;
+  $border-color: #345b88;
+  $brand-color: whitesmoke;
+
+  .header {
+    .brand {
+      .brand-name {
+        letter-spacing: -1px;
+      }
+      .brand-version {
+        font-size: 50%;
+      }
+      display: table-cell;
+      padding-left: 10px;
+      vertical-align: middle;
+      text-decoration: none;
+      color: $brand-color;
+      text-transform: lowercase;
+      font-weight: 100;
+      font-size: 120%;
+    }
+    display: table-row;
+    height: 50px;
+    font-family: $font-stack;
+    background-color: $primary-color;
+    border-color: $border-color;
+    border-bottom: 1px solid;
+    align-items: center;
+    padding: 0 10px;
+    user-select: none;
+  }
+</style>
