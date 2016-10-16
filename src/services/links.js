bebc04c16746ec5b03b0193ebd9197804c8279e6
Roy Mor <roy.mor173@gmail.com>
2016-10-16 20:55:35 +0300

    feat(Navbar): add links data service

---
+++ src/services/links.js
@@ -0,0 +1,17 @@
+export const links = [
+  {
+    icon: 'fa-archive',
+    description: 'Project',
+    to: '/project'
+  },
+  {
+    icon: 'fa-book',
+    description: 'Reports',
+    to: '/reports'
+  },
+  {
+    icon: 'fa-cubes',
+    description: 'Workers',
+    to: '/workers'
+  }
+];
