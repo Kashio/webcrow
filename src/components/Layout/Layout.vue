ba483dda6b7c149947ed37bcfddf3f16a0565625
Roy Mor <roy.mor173@gmail.com>
2016-10-16 21:03:01 +0300

    feat(Layout): add Layout component
    Layout component responsible for constructing the frame of how the website should look

---
+++ src/components/Layout/Layout.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="app-layout">
+    <header-component></header-component>
+    <div class="main">
+      <navbar-component>
+        <router-view name="menu" slot="menu"></router-view>
+      </navbar-component>
+      <main>
+        <router-view name="main"></router-view>
+      </main>
+    </div>
+  </div>
+</template>
+
+<script>
+  import Header from './Header/Header.vue';
+  import Navbar from './Navbar/Navbar.vue';
+  export default {
+    name: 'Layout',
+    components: {
+      'header-component': Header,
+      'navbar-component': Navbar
+    }
+  };
+</script>
+
+<style lang="sass" rel="stylesheet/scss" scoped>
+  .app-layout, .main{
+    display: table;
+    height: 100%;
+    width: 100%;
+  }
+</style>
