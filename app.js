d552aebbd6e0acb12b5d39f24373feedad498a72
Roy Mor <roy.mor173@gmail.com>
2016-12-11 02:38:00 +0200

    feat(server): add cors and pug views support

--- app.js
+++ app.js
@@ -5,11 +5,13 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var swaggerize = require('swaggerize-express');
+var cors = require('cors');
 
 var app = express();
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(cors());
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -19,6 +21,9 @@ app.use(swaggerize({
   api: path.join(__dirname, 'config/swagger.json'),
   handlers: path.join(__dirname, 'routes')
 }));
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
