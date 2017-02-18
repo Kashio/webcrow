5eba1cb92fe0bbb77a697c8bededf1e27e53be80
Roy Mor <roy.mor173@gmail.com>
2017-02-19 01:58:47 +0200

    client(tooltip): move tooltip directive into its own plugin and add a basic store

--- client/src/app.js
+++ client/src/app.js
@@ -1,10 +1,18 @@
 import Vue from 'vue';
 import './css/index.scss';
-import './directives';
+// import './directives';
 import { router } from './router';
+import { store } from './store';
+import './api';
+
+import { VueTooltip } from '@kashio/vue-tooltip';
+import '@kashio/vue-tooltip/dist/index.css';
+
+Vue.use(VueTooltip);
 
 export default new Vue({
   el: '#app',
   router,
+  store,
   render: h => h('router-view')
 });
