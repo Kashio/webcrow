978ebe98baf89d4314277a55f05783442e5e6eb3
Roy Mor <roy.mor173@gmail.com>
2017-02-19 01:54:48 +0200

    client(Item): add Item component

---
+++ client/src/components/Layout/Navbar/Section/Item/Item.vue
@@ -0,0 +1,17 @@
+<template>
+  <li class="menu-section-item">
+    <slot></slot>
+    </li>
+</template>
+
+<script>
+  export default {
+    name: 'Item'
+  };
+</script>
+
+<style lang="sass" rel="stylesheet/scss" scoped>
+  .menu-section-item {
+    margin-bottom: 5px;
+  }
+</style>
