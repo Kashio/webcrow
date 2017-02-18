4659d9bd27ff8b081784ea4326c2ba0b21927217
Roy Mor <roy.mor173@gmail.com>
2017-02-19 01:52:35 +0200

    server(project): fix project model and route
    Project model methods now use path.join() instead of util.format()
     Project route doesn't export default object

--- server/models/project.js
+++ server/models/project.js
@@ -1,19 +1,19 @@
 'use strict';
 
 import fs from 'fs';
-import util from 'util';
+import path from 'path';
 import mkdirp from 'mkdirp';
 import rmdir from 'rmdir';
 
 export default {
   create: (project, done) => {
-    mkdirp(util.format('%s%s', process.env.WEBCROW_HOME, project.id), done);
+    mkdirp(path.join(process.env.WEBCROW_HOME, project.id), done);
   },
   rename: (projectId, name, done) => {
-    fs.rename(util.format('%s%s', process.env.WEBCROW_HOME, projectId),
-      util.format('%s%s', process.env.WEBCROW_HOME, name), done);
+    fs.rename(path.join(process.env.WEBCROW_HOME, projectId),
+      path.join(process.env.WEBCROW_HOME, name), done);
   },
   delete: (projectId, done) => {
-    rmdir(util.format('%s%s', process.env.WEBCROW_HOME, projectId), done);
+    rmdir(path.join(process.env.WEBCROW_HOME, projectId), done);
   }
 };
--- server/routes/project.js
+++ server/routes/project.js
@@ -3,32 +3,64 @@
 import util from 'util';
 import dataProvider from '../models/project.js';
 
-export default {
-  post: (req, res, next) => {
-    dataProvider.create(req.body, (err) => {
-      if (err) {
-        next(err);
-        return;
-      }
-      res.status('201').send(util.format('project %s was created successfully', req.body.id));
-    });
-  },
-  put: (req, res, next) => {
-    dataProvider.rename(req.body.projectId, req.body.name, (err) => {
-      if (err) {
-        next(err);
-        return;
-      }
-      res.status('204').send(util.format('project %s was renamed to %s successfully', req.body.projectId, req.body.name));
-    });
-  },
-  delete: (req, res, next) => {
-    dataProvider.delete(req.body.projectId, (err) => {
-      if (err) {
-        next(err);
-        return;
-      }
-      res.status('204').send(util.format('project %s was deleted successfully', req.body.projectId));
-    });
-  }
+const post = (req, res, next) => {
+  dataProvider.create(req.body, (err) => {
+    if (err) {
+      next(err);
+      return;
+    }
+    res.status('201').send(util.format('project %s was created successfully', req.body.id));
+  });
 };
+
+const put = (req, res, next) => {
+  dataProvider.rename(req.body.projectId, req.body.name, (err) => {
+    if (err) {
+      next(err);
+      return;
+    }
+    res.status('204').send(util.format('project %s was renamed to %s successfully', req.body.projectId, req.body.name));
+  });
+};
+
+const remove = (req, res, next) => {
+  dataProvider.delete(req.body.projectId, (err) => {
+    if (err) {
+      next(err);
+      return;
+    }
+    res.status('204').send(util.format('project %s was deleted successfully', req.body.projectId));
+  });
+};
+
+export { post, put, remove };
+
+// export default {
+//   post: (req, res, next) => {
+//     dataProvider.create(req.body, (err) => {
+//       if (err) {
+//         next(err);
+//         return;
+//       }
+//       res.status('201').send(util.format('project %s was created successfully', req.body.id));
+//     });
+//   },
+//   put: (req, res, next) => {
+//     dataProvider.rename(req.body.projectId, req.body.name, (err) => {
+//       if (err) {
+//         next(err);
+//         return;
+//       }
+//       res.status('204').send(util.format('project %s was renamed to %s successfully', req.body.projectId, req.body.name));
+//     });
+//   },
+//   delete: (req, res, next) => {
+//     dataProvider.delete(req.body.projectId, (err) => {
+//       if (err) {
+//         next(err);
+//         return;
+//       }
+//       res.status('204').send(util.format('project %s was deleted successfully', req.body.projectId));
+//     });
+//   }
+// };
