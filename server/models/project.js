d99cf2a540d46f8044e3f7b476e9be361e736074
Roy Mor <roy.mor173@gmail.com>
2017-02-22 01:04:01 +0200

    feat(project): check for invalid directory names before creating a project

--- server/models/project.js
+++ server/models/project.js
@@ -4,22 +4,29 @@ import fs from 'fs';
 import path from 'path';
 import util from 'util';
 import fsp from 'fs-promise';
+import fspvr from 'fspvr';
 
 export default {
   create: (project, done) => {
     const directory = path.join(process.env.WEBCROW_HOME, project.id);
-    fsp.access(directory)
-      .then(() => {
-        done({
-          message: util.format('project %s already exists', project.id)
-        });
-      })
-      .catch((err) => {
-        fsp.ensureDir(directory)
-          .then(() => {
-            done();
-          });
+    if (!fspvr.isSegmentValid(project.id, true)) {
+      done({
+        message: util.format('%s is not a valid directory name', project.id)
       });
+    } else {
+      fsp.access(directory)
+        .then(() => {
+          done({
+            message: util.format('project %s already exists', project.id)
+          });
+        })
+        .catch(() => {
+          fsp.ensureDir(directory)
+            .then(() => {
+              done();
+            });
+        });
+    }
   },
   rename: (projectId, name, done) => {
     fs.rename(path.join(process.env.WEBCROW_HOME, projectId),
