ecffc1ac23594da6b4c10d27bb9fe27f1ce388cd
Roy Mor <roy.mor173@gmail.com>
2017-03-04 16:36:43 +0200

    feat(app.config): add client configuration file

---
+++ client/src/app.config.js
@@ -0,0 +1,16 @@
+export default {
+  toastSuccess: {
+    time: 5000,
+    borderRadius: 3,
+    fixedWidth: 300,
+    color: 'white',
+    backgroundColor: '#34b054'
+  },
+  toastFailure: {
+    time: 5000,
+    borderRadius: 3,
+    fixedWidth: 300,
+    color: 'white',
+    backgroundColor: '#b03434'
+  }
+};
